58d11012e4e7c92f170492b6989ab5ee2aa9e12e
NeiferDaviesHinestrozaMosquera-code <neiferdaviesmosq@outlook.com>
2024-08-13 21:40:19 -0500

    v0.0.6

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tstore/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:tstore/utils/theme/theme.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,12 +9,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
-      //home: MyHomePage(),
+      home: OnBoardingScreen(),
     );
   }
 }
